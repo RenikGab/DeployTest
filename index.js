b7871b2e7767efb9c17dae8a5e02638274c183c9
Габдулхаков Р.Ш <D:\The Bat!>
2021-07-15 10:00:04 +0300

    +about

--- index.js
+++ index.js
@@ -2,10 +2,26 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 80
 
-app.get(('/'), (req, res) => {
-  res.end('<h1>Hi from server</h1>')
+app.get(('/'), (req, res) => {  
+  res.end(`
+    <div>
+      <a href = "/">Home</a>
+      <a href = "/about">About</a>
+      <h1>Hi from server</h1>
+    </div>
+  `)
+})
+
+app.get(('/about'), (req, res) => {  
+  res.end(`
+    <div>
+      <a href = "/">Home</a>
+      <a href = "/about">About</a>
+      <h1>About</h1>
+    </div>
+  `)
 })
 
 app.listen(PORT, () => {
-  console.log("hi from server")
+  console.log("hi from server ")
 })
