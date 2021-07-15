305fa5dca69cae48afd4a34b7e5f6126e2429a4b
Габдулхаков Р.Ш <D:\The Bat!>
2021-07-15 09:43:24 +0300

    +server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi")
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 80
+
+app.get(('/'), (req, res) => {
+  res.end('<h1>Hi from server</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log("hi from server")
+})
